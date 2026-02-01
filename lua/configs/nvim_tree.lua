eb4d72b903817774a6b22bbfd1ca8b9ea905eac7
Ahmad Faisal <ahmad.faisalewy@gmail.com>
2026-02-01 11:13:54 +0700

    chore: reformat

--- lua/configs/nvim_tree.lua
+++ lua/configs/nvim_tree.lua
@@ -3,77 +3,79 @@
 
 local nvim_tree_ok, nvim_tree = pcall(require, "nvim-tree")
 if not nvim_tree_ok then
-    return
+	return
 end
 
-nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_tab = false,
-  hijack_cursor = true,
-  update_cwd = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-  },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    side = "left",
-    number = false,
-    relativenumber = false,
-    preserve_window_proportions = true,
-  },
-  renderer = {
-    highlight_git = true,
-    root_folder_modifier = ":t",
-    indent_markers = { enable = true },
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      }
-    }
-  }
-}
-
+nvim_tree.setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_tab = false,
+	hijack_cursor = true,
+	update_cwd = true,
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	git = {
+		enable = true,
+		ignore = false,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		side = "left",
+		number = false,
+		relativenumber = false,
+		preserve_window_proportions = true,
+	},
+	renderer = {
+		highlight_git = true,
+		root_folder_modifier = ":t",
+		indent_markers = { enable = true },
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+})
